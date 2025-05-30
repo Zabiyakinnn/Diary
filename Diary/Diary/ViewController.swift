79f5c42b6952ae2ed50a506e5bbcff5640526f49
Dmitry Zabiyakin <zabiyakin09@yandex.ru>
2025-05-30 10:57:38 +0300

    Start Project

---
+++ Diary/Diary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Diary
+//
+//  Created by Дмитрий Забиякин on 30.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Hello"
+        view.backgroundColor = .green
+    }
+
+
+}
+
